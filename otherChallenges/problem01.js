d6d916cae51784892eb27f12fb88bbaba5cb79fb
Matheusmottaa <matheusfelipe1918@gmail.com>
2022-06-01 22:38:50 -0300

    add problem 01

--- otherChallenges/problem01.js
+++ otherChallenges/problem01.js
@@ -17,8 +17,6 @@ function same(arr1, arr2){
   for(let val of arr2){ 
     frequencyCounter2[val] = (frequencyCounter2[val]||0) + 1; 
   }
-  console.log(frequencyCounter1); 
-  console.log(frequencyCounter2);
   for(let key in frequencyCounter1){ 
     if(!(key**2 in frequencyCounter2)){ 
       return false; 
