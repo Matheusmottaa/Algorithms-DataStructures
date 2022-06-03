90a9c336de6e8c8c1d5554b076cd72de5c047844
Matheusmottaa <matheusfelipe1918@gmail.com>
2022-06-02 22:57:16 -0300

    add problem 04

--- javascript/otherChallenges/problem04.js
+++ javascript/otherChallenges/problem04.js
@@ -10,7 +10,7 @@ function countUniqueValues(array){
   let i =0;
   for(let j=1; j<array.length; j++){ 
     if(array[i] !== array[j]){ 
-      i +=1; 
+      i++; 
       array[i] = array[j]; 
     }
   }
